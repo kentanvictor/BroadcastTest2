ba42ac0cbf3c2f850140ffef513f2ead80c191ed
kentanvictor <2030457926@qq.com>
2017-05-06 20:32:29 +0800

    完整版的利用广播的强制下线功能

--- app/src/main/java/com/example/dell/broadcasttest2/Activity/BaseActivity.java
+++ app/src/main/java/com/example/dell/broadcasttest2/Activity/BaseActivity.java
@@ -1,6 +1,12 @@
 package com.example.dell.broadcasttest2.Activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BaseActivity extends AppCompatActivity{
+    private ForceOffLineReceiver receiver;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -15,9 +22,47 @@ public class BaseActivity extends AppCompatActivity{
         ActivityCollector.addActivity(this);
     }
     @Override
+    protected void onResume()
+    {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.dell.broadcasttest2.FORCEOFFLINE");
+        receiver = new ForceOffLineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if(receiver!=null)
+        {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+    @Override
     protected void onDestroy()
     {
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+}
+class ForceOffLineReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Warning");
+        builder.setMessage("You are forced to be offline.Please try to login again.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ActivityCollector.finishAll();//销毁所有的活动
+                Intent intent = new Intent(context,LoginActivity.class);
+                context.startActivity(intent);//重新启动LoginActivity
+            }
+        });
+        builder.show();
     }
 }
--- app/src/main/java/com/example/dell/broadcasttest2/BroadCastReceiver/MainActivity.java
+++ app/src/main/java/com/example/dell/broadcasttest2/BroadCastReceiver/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.dell.broadcasttest2.BroadCastReceiver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.dell.broadcasttest2.Activity.BaseActivity;
 import com.example.dell.broadcasttest2.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button forceOffLIne = (Button) findViewById(R.id.force_offline);
+        forceOffLIne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.dell.broadcasttest2.FORCEOFFLINE");
+                sendBroadcast(intent);
+            }
+        });
     }
 }
