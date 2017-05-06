577dd710520ceb72479a13c8f1bc20101827f9a3
kentanvictor <2030457926@qq.com>
2017-05-06 20:02:15 +0800

    完整模拟登录功能

--- app/src/main/java/com/example/dell/broadcasttest2/Activity/LoginActivity.java
+++ app/src/main/java/com/example/dell/broadcasttest2/Activity/LoginActivity.java
@@ -1,15 +1,44 @@
 package com.example.dell.broadcasttest2.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.dell.broadcasttest2.BroadCastReceiver.MainActivity;
 import com.example.dell.broadcasttest2.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity
+{
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                //如果账号是admin并且密码是123456，就认为登陆成功
+                if(account.equals("admin")&&password.equals("123456"))
+                {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this, "count or password is invalid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
