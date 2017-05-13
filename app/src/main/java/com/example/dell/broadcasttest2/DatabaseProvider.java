faf39990944831c02dd137d613bb2b9dff856ef9
kentanvictor <2030457926@qq.com>
2017-05-13 17:28:14 +0800

    实现跨数据共享(未完成)

--- app/src/main/java/com/example/dell/broadcasttest2/DatabaseProvider.java
+++ app/src/main/java/com/example/dell/broadcasttest2/DatabaseProvider.java
@@ -2,10 +2,17 @@ package com.example.dell.broadcasttest2;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
 public class DatabaseProvider extends ContentProvider {
+    public static final int BOOK_DIR = 0;
+    public static final int BOOK_ITEM = 1;
+    public static final int CATEGORY_DIR = 2;
+    public static final int CATEGORY_ITEM = 3;
+    public static final String AUTHORITY = "com.example.dell.databasetest.provider";
+    private static UriMatcher uriMatcher;
     public DatabaseProvider() {
     }
 
